80bb65cf5b4d263d85ed0369f56b5248818295c6
Cats-n-coffee <lucie.macaigne@gmail.com>
2021-11-07 12:51:41 -0500

    husky test

--- src/components/body/Body.tsx
+++ src/components/body/Body.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 
 export default function Body() {
-	return <main className="body">Body component</main>;
+	return <main className="body">Body component
+		<p>test for husky</p>
+	</main>;
 }
