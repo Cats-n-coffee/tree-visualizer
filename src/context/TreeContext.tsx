9010b63f506af225736420f2f098f80388e6a3d3
Cats-n-coffee <lucie.macaigne@gmail.com>
2021-11-07 22:47:24 -0500

    basic tree context

---
+++ src/context/TreeContext.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+
+interface TreeNode {
+    name: string,
+    props: Array<PropFormValue>,
+    state: Array<StateFormValue>,
+    parent: string,
+    children?: Array<TreeNode>
+}
+
+// setTree optional, otherwise 'value' prop is missing a property
+type TreeContextState = {
+    tree: Array<TreeNode>,
+    setTree?: React.Dispatch<React.SetStateAction<Array<TreeNode>>>
+}
+
+const treeContextDefault: TreeContextState = {
+    tree: [],
+    setTree: () => {}
+}
+
+const TreeContext = React.createContext<TreeContextState>(treeContextDefault);
+TreeContext.displayName = 'TreeContext';
+
+export default function TreeContextProvider({ children }: AppProps) {
+    const [tree, setTree] = React.useState<TreeNode[]>([]);
+
+    const value = {tree};
+
+    return <TreeContext.Provider value={value}>{children}</TreeContext.Provider>
+}
