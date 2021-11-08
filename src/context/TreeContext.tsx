b9f4484e55f5ea920f5ac9120cdf579925979b7e
Cats-n-coffee <lucie.macaigne@gmail.com>
2021-11-08 15:30:41 -0500

    added usecontext hook for tree context

--- src/context/TreeContext.tsx
+++ src/context/TreeContext.tsx
@@ -1,31 +1,41 @@
-import * as React from 'react';
+import * as React from "react";
 
 interface TreeNode {
-    name: string,
-    props: Array<PropFormValue>,
-    state: Array<StateFormValue>,
-    parent: string,
-    children?: Array<TreeNode>
+  name: string;
+  props: Array<PropFormValue>;
+  state: Array<StateFormValue>;
+  parent: string;
+  children?: Array<TreeNode>;
 }
 
 // setTree optional, otherwise 'value' prop is missing a property
 type TreeContextState = {
-    tree: Array<TreeNode>,
-    setTree?: React.Dispatch<React.SetStateAction<Array<TreeNode>>>
-}
+  tree: Array<TreeNode>;
+  setTree?: React.Dispatch<React.SetStateAction<Array<TreeNode>>>;
+};
 
 const treeContextDefault: TreeContextState = {
-    tree: [],
-    setTree: () => {}
-}
+  tree: [],
+  setTree: () => {},
+};
 
 const TreeContext = React.createContext<TreeContextState>(treeContextDefault);
-TreeContext.displayName = 'TreeContext';
+TreeContext.displayName = "TreeContext";
 
 export default function TreeContextProvider({ children }: AppProps) {
-    const [tree, setTree] = React.useState<TreeNode[]>([]);
+  const [tree, setTree] = React.useState<TreeNode[]>([]);
 
-    const value = {tree};
+  const value = { tree };
 
-    return <TreeContext.Provider value={value}>{children}</TreeContext.Provider>
+  return <TreeContext.Provider value={value}>{children}</TreeContext.Provider>;
+}
+
+export function useTreeContext() {
+  const tree = React.useContext(TreeContext);
+
+  if (!tree) {
+    throw new Error("Hook needs to be used inside the TreeContextProvider");
+  }
+
+  return tree;
 }
