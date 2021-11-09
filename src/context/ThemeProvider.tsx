64f4cee45353981f96152812df32db350061a767
Cats-n-coffee <lucie.macaigne@gmail.com>
2021-11-09 16:26:08 -0500

    removed eslint env rules

--- src/context/ThemeProvider.tsx
+++ src/context/ThemeProvider.tsx
@@ -1,42 +1,43 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 import * as React from 'react';
 
 type ThemeContextState = {
-	theme: string;
-	setTheme: React.Dispatch<React.SetStateAction<string>>;
+  theme: string;
+  setTheme: React.Dispatch<React.SetStateAction<string>>;
 };
 
 const contextDefaultValue: ThemeContextState = {
-	theme: '',
-	setTheme: () => {},
+  theme: '',
+  setTheme: () => {},
 };
 
 const ThemeContext =
-	React.createContext<ThemeContextState>(contextDefaultValue);
+  React.createContext<ThemeContextState>(contextDefaultValue);
 ThemeContext.displayName = 'ThemeContext';
 
 export default function ThemeProvider({ children }: AppProps) {
-	const [theme, setTheme] = React.useState(
-		() => window.localStorage.getItem('theme') || 'light'
-	);
+  const [theme, setTheme] = React.useState(
+    () => window.localStorage.getItem('theme') || 'light'
+  );
 
-	const value = { theme, setTheme };
+  const value = { theme, setTheme };
 
-	React.useEffect(() => {
-		document.body.dataset.theme = theme;
-		localStorage.setItem('theme', theme);
-	}, [theme]);
+  React.useEffect(() => {
+    document.body.dataset.theme = theme;
+    localStorage.setItem('theme', theme);
+  }, [theme]);
 
-	return (
-		<ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
-	);
+  return (
+    <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+  );
 }
 
 export function useThemeProvider() {
-	const theme = React.useContext(ThemeContext);
+  const theme = React.useContext(ThemeContext);
 
-	if (!theme) {
-		throw new Error('useThemeProvider must be used inside ThemeProvider');
-	}
+  if (!theme) {
+    throw new Error('useThemeProvider must be used inside ThemeProvider');
+  }
 
-	return theme;
+  return theme;
 }
