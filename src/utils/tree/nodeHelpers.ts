041be9b14225a3c169e32a15d6d082ce717c6c7f
Cats-n-coffee <lucie.macaigne@gmail.com>
2021-11-10 09:46:09 -0500

    removed console.log, changed interface declaration, removed duplicate dependencies

--- src/utils/tree/nodeHelpers.ts
+++ src/utils/tree/nodeHelpers.ts
@@ -3,12 +3,12 @@ export function findNodeAndInsert(
   newNode: TreeNode,
   tree: TreeNode
 ) {
+  // If the names match, we found the parent to which we add the child to
   if (tree.name === parentName) {
-    console.log("adding a child to the parent", tree);
     tree.allChildren.push(newNode);
   } else {
+    // If the names don't match, we keep looking in the tree
     for (let i = 0; i < tree.allChildren.length; i += 1) {
-      console.log("we have to find the parent");
       findNodeAndInsert(parentName, newNode, tree.allChildren[i]);
     }
   }
