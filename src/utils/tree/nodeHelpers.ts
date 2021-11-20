e924dc3c79fd0a9c79eaef738e0b82efde53efb9
Cats-n-coffee <lucie.macaigne@gmail.com>
2021-11-20 10:48:24 -0500

    put array in variable for for loop

--- src/utils/tree/nodeHelpers.ts
+++ src/utils/tree/nodeHelpers.ts
@@ -14,7 +14,7 @@ export function findNodeAndInsert({
     tree.allChildren.push(newNode);
   } else {
     // If the names don't match, we keep looking in the tree
-    for (let i = 0; i < tree.allChildren.length; i += 1) {
+    for (let i = 0, len = tree.allChildren.length; i < len; i += 1) {
       findNodeAndInsert({ parentName, newNode, tree: tree.allChildren[i] });
     }
   }
