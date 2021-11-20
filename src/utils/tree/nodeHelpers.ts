eb8b8df3a22d1963fff9af9b97015044bd880c78
Cats-n-coffee <lucie.macaigne@gmail.com>
2021-11-20 10:45:49 -0500

    interface for function agrs

--- src/utils/tree/nodeHelpers.ts
+++ src/utils/tree/nodeHelpers.ts
@@ -1,15 +1,21 @@
-export function findNodeAndInsert(
-  parentName: string | null,
-  newNode: TreeNode,
-  tree: TreeNode
-) {
+interface FindAndInsert {
+  parentName: string | null;
+  newNode: TreeNode;
+  tree: TreeNode;
+}
+
+export function findNodeAndInsert({
+  parentName,
+  newNode,
+  tree,
+}: FindAndInsert) {
   // If the names match, we found the parent to which we add the child to
   if (tree.name === parentName) {
     tree.allChildren.push(newNode);
   } else {
     // If the names don't match, we keep looking in the tree
     for (let i = 0; i < tree.allChildren.length; i += 1) {
-      findNodeAndInsert(parentName, newNode, tree.allChildren[i]);
+      findNodeAndInsert({ parentName, newNode, tree: tree.allChildren[i] });
     }
   }
   return [tree];
